2aac8bb3ebfa5a89db5bd00b25343dc173bf6e47
Kim <kimberlyasharpe@gmail.com>
2016-11-16 13:13:33 -0500

    Script talk
    Made Destroy script access variable from RandomSpawn Script

--- Assets/Scripts/DestroyAnimal.cs
+++ Assets/Scripts/DestroyAnimal.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class DestroyAnimal : MonoBehaviour {
 
+	public GameObject spawnerObject;
+
+	private RandomSpawn randomSpawnAcess;
+	private float randomSpawnSeconds;
+
 	// Use this for initialization
 	void Start () {
-		Destroy (gameObject, 6);
+
+		randomSpawnAcess = spawnerObject.GetComponent<RandomSpawn>();
+		randomSpawnSeconds = randomSpawnAcess.spawnSeconds;
+		Debug.Log (randomSpawnSeconds);
+
+		Destroy (gameObject, randomSpawnSeconds);
 	}
 }
